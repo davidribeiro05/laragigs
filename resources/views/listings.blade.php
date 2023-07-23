d82cf2dcaee7f44cf08af84d6c9a2e86fee7735a
David Ribeiro <david.ribeiro05@hotmail.com>
2023-07-23 11:09:27 -0300

    refactor: layout e rotas

--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -7,7 +7,7 @@
 
 @foreach($listings as $listing)
 <h2>
-    <a href="/listings/{{$listing['id]}}">{{ $listing['title']}}</a>
+    <a href="/listings/{{ $listing['id'] }}">{{ $listing['title']}}</a>
 </h2>
 <p>{{ $listing['description']}}</p>
 @endforeach
